6f44d38ef21c29ce74b1bce41c6f148b02471dd9
Nicollas <nicollasmatheus37@gmail.com>
2019-03-27 19:52:36 -0300

    baseDAO att

--- src/base/BaseDAO.java
+++ src/base/BaseDAO.java
@@ -11,15 +11,15 @@ public abstract class BaseDAO {
 	
 	private String sql = "";
 	
-	protected abstract List<Object> index() throws SQLException;
-	
-	protected abstract Object view(int id) throws SQLException;
-	
-	protected abstract void store() throws SQLException;
-	
-	protected abstract void update() throws SQLException;
-	
-	protected abstract void destroy() throws SQLException;
+//	protected abstract List<Object> index() throws SQLException;
+//	
+//	protected abstract Object view(int id) throws SQLException;
+//	
+//	protected abstract void store() throws SQLException;
+//	
+//	protected abstract void update() throws SQLException;
+//	
+//	protected abstract void destroy() throws SQLException;
 	
 	protected void _set(PreparedStatement preparedStatement, int position, Object params) throws SQLException {
 		if(params == null) {
@@ -46,6 +46,26 @@ public abstract class BaseDAO {
 	
 	protected BaseDAO orderBy(String statement) {
 		this.setSql(this.getSql() + "ORDER BY " + statement);
+		return this;
+	}
+	
+	protected BaseDAO join(String statement) {
+		this.setSql(this.getSql() + "JOIN " + statement);
+		return this;
+	}
+	
+	protected BaseDAO innerJoin(String statement) {
+		this.setSql(this.getSql() + "INNER JOIN " + statement);
+		return this;
+	}
+	
+	protected BaseDAO leftJoin(String statement) {
+		this.setSql(this.getSql() + "LEFT JOIN " + statement);
+		return this;
+	}
+	
+	protected BaseDAO rightJoin(String statement) {
+		this.setSql(this.getSql() + "RIGHT JOIN " + statement);
 		return this;
 	}
 
